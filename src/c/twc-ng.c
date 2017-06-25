29946c34901710c65b5eee1024ad6677cde25a9a
EEMыч <mikhaylov.eugene@gmail.com>
2017-06-25 13:57:15 +0300

    init Clay for Pebble support

--- src/c/twc-ng.c
+++ src/c/twc-ng.c
@@ -1,8 +1,12 @@
 #include <pebble.h>
 #include "twc-ng.h"
 
+ClaySettings settings;
+
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+
+
 
 //---------bitmaps
 static BitmapLayer *s_top_panel_layer_1;
@@ -26,6 +30,28 @@ static GBitmap *s_bt_connected;
 //   window_single_click_subscribe(BUTTON_ID_UP, prv_up_click_handler);
 //   window_single_click_subscribe(BUTTON_ID_DOWN, prv_down_click_handler);
 // }
+static void prv_default_settings() {
+  settings.ClockFormat = true;
+}
+
+static void prv_load_settings() {
+  // Load the default settings
+  prv_default_settings();
+  // Read settings from persistent storage, if they exist
+  persist_read_data(SETTINGS_KEY, &settings, sizeof(settings));
+}
+
+// Save the settings to persistent storage
+static void prv_save_settings() {
+  persist_write_data(SETTINGS_KEY, &settings, sizeof(settings));
+  // Update the display based on new settings
+  prv_update_display();
+}
+
+static void prv_update_display() {
+  update_time();
+}
+
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_chanhed) {
   update_time();
@@ -54,6 +80,7 @@ static void prv_window_load(Window *window) {
 //---adding layers
   layer_add_child(window_layer, bitmap_layer_get_layer(s_top_panel_layer_1));
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+  prv_update_display();
 }
 
 static void prv_window_unload(Window *window) {
@@ -62,7 +89,19 @@ static void prv_window_unload(Window *window) {
   bitmap_layer_destroy(s_top_panel_layer_1);
 }
 
+static void prv_inbox_received_handler(DictionaryIterator *iter, void *context) {
+  Tuple *is_clock_24h_t = dict_find(iter, MESSAGE_KEY_ClockFormat);
+  if(is_clock_24h_t) {
+    settings.ClockFormat= is_clock_24h_t->value->int32 == 1;
+  }
+  prv_save_settings();
+}
 static void prv_init(void) {
+  prv_load_settings();
+
+  app_message_register_inbox_received(prv_inbox_received_handler);
+  app_message_open(128, 128);
+
   s_main_window = window_create();
 //  window_set_click_config_provider(s_window, prv_click_config_provider);
   window_set_window_handlers(s_main_window, (WindowHandlers) {
@@ -70,6 +109,10 @@ static void prv_init(void) {
     .unload = prv_window_unload,
   });
   const bool animated = true;
+
+  // Open AppMessage connection
+
+
   window_stack_push(s_main_window, animated);
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
   update_time();
@@ -93,7 +136,7 @@ static void update_time() {
     struct tm *tick_time = localtime(&temp);
 
     static char s_buffer[8];
-    strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
+    strftime(s_buffer, sizeof(s_buffer), settings.ClockFormat ?
       "%H:%M" : "%I:%M", tick_time);
     text_layer_set_text(s_time_layer, s_buffer);
 }
---
+++ src/pkjs/config.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    "type": "heading",
+    "defaultValue": "TWC-ng Configuration"
+  },
+  {
+    "type": "section",
+    "items":
+    [
+      {
+        "type": "heading",
+        "defaultValue": "Time format"
+      },
+      {
+        "type": "toggle",
+        "messageKey": "ClockFormat",
+        "label": "Use 24h format",
+        "defaultValue": true
+      }
+    ]
+  },
+  {
+  "type": "submit",
+  "defaultValue": "Save Settings"
+  }
+];
