231a4cae85a0a6651ed0ac848cb1169f27130629
EEMыч <mikhaylov.eugene@gmail.com>
2017-06-25 11:05:08 +0300

    init bitmap test

--- src/c/twc-ng.c
+++ src/c/twc-ng.c
@@ -4,6 +4,11 @@
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 
+//---------bitmaps
+static BitmapLayer *s_top_panel_layer_1;
+
+static GBitmap *s_bt_connected;
+
 // static void prv_select_click_handler(ClickRecognizerRef recognizer, void *context) {
 //   text_layer_set_text(s_time_layer, "Select");
 // }
@@ -30,14 +35,31 @@ static void prv_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
+//-----bitmaps
+  s_bt_connected = gbitmap_create_with_resource(RESOURCE_ID_BLUETOOTH_CONNECTED);
+
+//----bitmap layers
+  s_top_panel_layer_1 = bitmap_layer_create((GRect(0, 0, 20, 20)));
+
+//----text layers
   s_time_layer = text_layer_create(GRect(0, 72, bounds.size.w, 20));
+
+//----set bitmaps
+  bitmap_layer_set_bitmap(s_top_panel_layer_1, s_bt_connected);
+
+//----set text
   text_layer_set_text(s_time_layer, "00:00");
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+
+//---adding layers
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_top_panel_layer_1));
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 }
 
 static void prv_window_unload(Window *window) {
   text_layer_destroy(s_time_layer);
+  gbitmap_destroy(s_bt_connected);
+  bitmap_layer_destroy(s_top_panel_layer_1);
 }
 
 static void prv_init(void) {
@@ -72,6 +94,6 @@ static void update_time() {
 
     static char s_buffer[8];
     strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
-      "%M:%S" : "%M:%S", tick_time);
+      "%H:%M" : "%I:%M", tick_time);
     text_layer_set_text(s_time_layer, s_buffer);
 }
