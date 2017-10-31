03307e4a5fa95457a84f35244a64f39b166c9467
EEMыч <hero.nospam@gmail.com>
2017-10-31 21:00:19 +0300

    fix typo

--- src/pkjs/locales/ru.js
+++ src/pkjs/locales/ru.js
@@ -57,6 +57,6 @@ module.exports = {
     "6 hours" : "6ти часовой",
     "disabled" : "отключен",
     "Shake your Pebble to view forecast. Set timeout for auto switch back." : "Потрясите часами, чтобы переключиться на окно показа прогноза погоды. Выставите таймаут в секундах ниже для автоматического переключения назад.",
-    "Switch back timeout in seconds" : "Переключаться назад автоматически через..."
+    "Switch back timeout in seconds" : "Переключаться назад автоматически через...",
     "You can support developer by donating via <a href='https://www.paypal.me/eugenemikhaylov'>Paypal</a>" : "Вы можете поддержать разработчика, сделав перевод через <a href='https://www.paypal.me/eugenemikhaylov'>Paypal</a>"
   };
