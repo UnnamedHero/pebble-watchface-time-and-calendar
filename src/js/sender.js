6a4228d66597dc0b64cc3e21146e024e091eedb8
UnnamedHero <hero.nospam@gmail.com>
2018-09-06 10:35:30 +0300

    refactor to es6

--- src/js/sender.js
+++ src/js/sender.js
@@ -1,24 +1,24 @@
-var timeout = 5000;
-var maxAttempts = 5;
+const timeout = 5000;
+const maxAttempts = 5;
 
-function sender(data, attempt) {
-	if (attempt > maxAttempts) {
-		console.log('Message send FAILED');
-		return;
-	}
-    var cb = function (currAttempt, data, res, error) {        
-        var nextAttempt = currAttempt + 1;
-        console.log('Error sending data to Pebble! Next Attempt:'+ nextAttempt+' in '+timeout+'ms');
-        setTimeout(function() {     
-            sender(data, nextAttempt);
-        }, timeout);        
-    };
-    var thisCb = cb.bind(undefined, attempt, data);
-	Pebble.sendAppMessage(data, function(e) {
-     	console.log('Data sent to Pebble successfully!');
-     }, thisCb);  
-}
+const sender = (data, attempt) => {
+  if (attempt > maxAttempts) {
+    console.log('Message send FAILED');
+    return;
+  }
 
-exports.send = function(data) {
-	sender(data, 1);
+  const onSuccess = () => {
+    console.log('Data sent to Pebble successfully!');
+  };
+
+  const onError = (nextAttempt) => {
+    console.log(`Error sending data to Pebble! Next Attempt: ${nextAttempt} in ${timeout} ms`);
+    setTimeout(() => {
+      sender(data, nextAttempt);
+    }, timeout);
+  };
+
+  Pebble.sendAppMessage(data, onSuccess, onError.bind(undefined, attempt + 1)); //eslint-disable-line
 };
+
+export default data => sender(data, 1);
