38ca60891c320deb590399ad5eb482be872bec23
UnnamedHero <hero.nospam@gmail.com>
2018-09-13 17:58:23 +0300

    fix sending error object

--- src/js/weather/index.js
+++ src/js/weather/index.js
@@ -3,24 +3,26 @@ import request from '../lib/request';
 import makePebbleWeatherObject from './pebbleWeatherObject';
 import { messages } from './weatherAttributeTables';
 
+// TODO: check for malformed and empty response from weather provider
+
 export default async (options) => {
   const provider = getProvider(options.provider);
   if (!provider) {
-    return makePebbleWeatherObject(messages.weather_disabled);
+    return makePebbleWeatherObject({ WeatherError: messages.weather_disabled });
   }
 
   if (options.apiKey === 'not_set' || options.apiKey === '') {
-    return makePebbleWeatherObject(messages.api_key_not_set);
+    return makePebbleWeatherObject({ WeatherError: messages.api_key_not_set });
   }
 
   if (options.apiKey === 'invalid') {
-    return makePebbleWeatherObject(messages.api_key_invalid);
+    return makePebbleWeatherObject({ WeatherError: messages.api_key_invalid });
   }
 
   const { errors, url } = await provider.makeUrl(options);
   if (errors.message) {
     console.log(`makeUrl: ${errors.message}`);
-    return makePebbleWeatherObject(messages.location_error);
+    return makePebbleWeatherObject({ WeatherError: messages.location_error }, options.type);
   }
   let weatherResponse;
   try {
@@ -28,20 +30,20 @@ export default async (options) => {
     weatherResponse = await request(url);
   } catch (e) {
     console.log(`error getting weather ${e}`);
-    return makePebbleWeatherObject(messages.unknown_error);
+    return makePebbleWeatherObject({ WeatherError: messages.unknown_error }, options.type);
   }
   const weatherPOJO = JSON.parse(weatherResponse);
 
   const code = String(weatherPOJO.cod);
   console.log(`response code ${code} at ${options.type}`);
   if (code === '401') {
-    return makePebbleWeatherObject(messages.api_key_invalid);
+    return makePebbleWeatherObject({ WeatherError: messages.api_key_invalid });
   }
   if (code === '400') {
-    return makePebbleWeatherObject(messages.invalid_location_id);
+    return makePebbleWeatherObject({ WeatherError: messages.invalid_location_id });
   }
   if (code !== '200') {
-    return makePebbleWeatherObject(messages.unknown_error);
+    return makePebbleWeatherObject({ WeatherError: messages.unknown_error });
   }
   console.log('making weather object');
   const weatherObj = provider.makeWeatherObj(options, weatherPOJO);
