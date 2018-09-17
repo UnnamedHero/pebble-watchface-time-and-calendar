94e9b8b44eed725e843d13bd4afb7fc8bec1fead
UnnamedHero <hero.nospam@gmail.com>
2018-09-17 11:11:25 +0300

    move geoposition options to lib, increase maximum age

--- src/js/lib/geoposition.js
+++ src/js/lib/geoposition.js
@@ -1,4 +1,4 @@
-export default (options = {}) => { // eslint-disable-line
+export default (options = { timeout: 10000, maximumAge: 1800000 }) => { // eslint-disable-line
   return new Promise((resolve, reject) => {
     navigator.geolocation.getCurrentPosition(resolve, reject, options);
   });
