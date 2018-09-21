9d36dee54419e684cbf394a9a3443e9e07977bd2
UnnamedHero <hero.nospam@gmail.com>
2018-09-21 10:17:52 +0300

    fix MINUTES_PER_HOUR redeclaration

--- src/c/utils/include/timelib.h
+++ src/c/utils/include/timelib.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <stdbool.h>
-#define MINUTES_PER_HOUR 60
+
+#if !defined(MINUTES_PER_HOUR)
+  #define MINUTES_PER_HOUR 60
+#endif
 
 bool is_within_range(int now, int begin, int end);
 int get_mins(int hours, int mins);
