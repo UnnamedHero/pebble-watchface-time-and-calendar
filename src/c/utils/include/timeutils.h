f8f74a99d565ba8af27e3c5dab2dd3b7e2ee354e
UnnamedHero <hero.nospam@gmail.com>
2018-10-18 17:47:38 +0300

    remove unused code

--- src/c/utils/include/timeutils.h
+++ src/c/utils/include/timeutils.h
@@ -11,5 +11,3 @@ bool is_quiet_time();
 void get_currect_time(DT_FORMAT, char *buffer);
 void get_current_date(char *format, char *out);
 bool is_time_to(uint32_t , PERIOD);
-
-int get_min(int hours, int mins);
